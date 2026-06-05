38a4a0f758670e99e4a99a74f7da669b1fcb776e
delegate2027 <dictation2027@protonmail.com>
2026-06-05 16:09:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -90,7 +90,6 @@ async function loadRSS() {
         : pubDate.toLocaleDateString("fr-FR", {
             day: "2-digit",
             month: "2-digit",
-            year: "numeric",
             hour: "2-digit",
             minute: "2-digit",
           });
